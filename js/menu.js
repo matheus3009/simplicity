70e733da2291bee69605234ad8a0b40d12632c73
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-04-30 16:50:34 -0300

    continuação

--- js/menu.js
+++ js/menu.js
@@ -2,18 +2,26 @@
 
 /*  Selecionando o elemento que acionará a lista de links do menu */
 
-const botaoMenu = document.querySelector("nav h2");
+const botaoMenu = document.querySelector("nav h2 a");
 
 // Selecionando a lista de links/menu
 const listaDeLinks = document.querySelector(".links-menu");
 
 /*  Monitorando o evento de click do botaoMenu */
 
-botaoMenu.addEventListener("click", function(event){
+botaoMenu.addEventListener("click", function (event) {
 
     /* "Previnir/Anular" o evento padão do link de redirecionamento/recarregamento da página */
     event.preventDefault();
     console.log("beleza");
 
     listaDeLinks.classList.toggle("aberto")
+
+
+    if (listaDeLinks.classList.contains("aberto")) {
+        botaoMenu.innerHTML = "Fechar &times;"
+    } else {
+        botaoMenu.innerHTML = "Menu &equiv;"
+    }
+
 });
